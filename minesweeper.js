d2592d4561d98d8380e04531dc49a3a675cb6ab6
Alex <=ahick090@gmail.com>
2018-05-03 15:07:57 -0400

    Removed console logs for testing

--- minesweeper.js
+++ minesweeper.js
@@ -72,7 +72,6 @@ function mineClick(tileCount, button) {
 
 function tileClick(tileCount, height, width, button) {
 
-console.log("Tilecount: " + tileCount);
 var tile = byId(tileCount);
 
 //left click - reveal tile
@@ -125,7 +124,6 @@ if (button == 0 && !tile.classList.contains("flag")) {
 		checkTiles = [parseInt(tileCount) + parseInt(width), parseInt(tileCount) + parseInt(width) - 1, tileCount - 1];
 	}
 
-	console.log("CheckTiles: " + checkTiles);
 	for (var num = 0; num < checkTiles.length; num++) {
 		var t = byId(checkTiles[num]);
 		//console.log(t);
@@ -174,7 +172,6 @@ if (button == 0 && !tile.classList.contains("flag")) {
 
 	// Recursive Tileclicks for all adjacent tiles with 0 adjacent mines
 	if (adjacentMines == 0) {
-		//console.log("Adjacent mines are 0 on tile " + tile.id);
 		for (var i = 0; i < checkTiles.length; i++) {
 			var t = byId(checkTiles[i]);
 			
@@ -224,7 +221,6 @@ function placeMines(mines, height, width) {
 			n--;
 		}
 	}
-	console.log(mineArray);
 }
 
 function placeNonMines(height, width) {
@@ -239,7 +235,6 @@ function placeNonMines(height, width) {
 				var tile = byId(tileCount);
 				var n = document.createElement("div");
 				n.className = "clean";
-				//console.log("Appending child: " + tileCount + "   height, width: " + height + ", " + width);
 				tile.appendChild(n);
 			}
 		}
@@ -251,7 +246,6 @@ function checkWin(tilesClicked) {
 	var nonMines = document.getElementsByClassName("clean");
 
 	if(tilesClicked == nonMines.length && mineCount == 0) {
-		//console.log("win");
 		setTimeout(function() {
 			gameOver(1);
 		}, 700);
